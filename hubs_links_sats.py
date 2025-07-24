583ca0540b6a69b0c22e92f3671c2d4a027897f4
sudarshan710 <sudarshanhope710@gmail.com>
2025-07-25 01:47:49 +0530

    ttravel-schemaevol-loads-hashcollision-surkey-

--- hubs_links_sats.py
+++ hubs_links_sats.py
@@ -1,7 +1,8 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import current_timestamp, lit, concat_ws, sha2, col
+from pyspark.sql.functions import current_timestamp, lit, concat_ws, sha2, col, expr
 import os
 from logger import loggerF
+from delta.tables import DeltaTable
 
 logger = loggerF(__name__)
 
@@ -25,9 +26,26 @@ def hubCreator(sourcePath, tableName, iD):
                   .dropDuplicates([iD]) \
                   .withColumn(f"{tableName}_HK", sha2(concat_ws("||", col(iD).cast("string")), 256)) \
                   .withColumn("load_time", current_timestamp())
+        
+        condition = f"target.{tableName}_HK = source.{tableName}_HK"
 
-        hub_df.write.format("delta").mode("overwrite").save(output_path)
-        logger.debug(f"Hub_{tableName} successfully created!")
+        if DeltaTable.isDeltaTable(spark, output_path):
+            delta_table = DeltaTable.forPath(spark, output_path)
+            target_df = delta_table.toDF()
+            same_count = hub_df.count() == target_df.count()
+            same_keys = hub_df.select(f"{tableName}_HK").subtract(target_df.select(f"{tableName}_HK")).isEmpty()
+
+            if same_count and same_keys:
+                logger.info(f"No changes detected for Hub_{tableName}. Skipping merge.")
+                return
+            delta_table.alias("target").merge(
+                source=hub_df.alias("source"),
+                condition=expr(condition)
+            ).whenNotMatchedInsertAll().execute()
+            logger.debug(f"Hub_{tableName} successfully updated/merged!")
+        else:
+            hub_df.write.format("delta").mode("overwrite").save(output_path)
+            logger.debug(f"Hub_{tableName} successfully created!")
     except Exception as e:
         logger.error(f"Hub {tableName} creating failed...", exc_info=True)
         raise
@@ -45,10 +63,27 @@ def linkCreator(sourcePath, tableName, linkName, idsList):
         link_df = raw_df.select(*idsList).dropDuplicates() \
                         .withColumn(linkName+"_HK", sha2(concat_ws("||", *[col(c).cast("string") for c in idsList]), 256)) \
                         .withColumn("load_date", current_timestamp()) \
-                        .withColumn("source", lit(tableName + ".csv"))  
-                
-        link_df.write.format("delta").mode("overwrite").save(output_path)
-        logger.debug(f"Link {linkName} successfully created!")
+                        .withColumn("source", lit(f"{tableName}"))
+        
+        condition = f"target.`{linkName}_HK` = sourceLink.`{linkName}_HK`"
+
+        if DeltaTable.isDeltaTable(spark, output_path):
+            delta_table = DeltaTable.forPath(spark, output_path)
+            target_df = delta_table.toDF()
+            same_count = link_df.count() == target_df.count()
+            same_keys = link_df.select(f"{linkName}_HK").subtract(target_df.select(f"{linkName}_HK")).isEmpty()
+
+            if same_count and same_keys:
+                logger.info(f"No changes detected for link_{linkName}. Skipping merge.")
+                return
+            delta_table.alias("target").merge(
+                source=link_df.alias("sourceLink"),
+                condition=expr(condition)
+            ).whenNotMatchedInsertAll().execute()
+            logger.debug(f"Link {linkName} successfully updated/merged!")
+        else:
+            link_df.write.format("delta").mode("overwrite").save(output_path)
+            logger.debug(f"Link {linkName} successfully created!")
     except Exception as e: 
         logger.error(f"Link {linkName} creating failed...", exc_info=True)
         raise
@@ -72,9 +107,25 @@ def satCreator(sourcePath, tableName, colsList):
             .withColumn("load_date", current_timestamp())
         
         sat_df = sat_df.dropDuplicates([f"{tableName}_HK", "hash_diff"])
-        
-        sat_df.write.format("delta").mode("overwrite").save(output_path)
-        logger.debug(f"Satellite {satName} successfully created!")
+
+        condition = f"target.{tableName}_HK = sourceSat.{tableName}_HK AND target.hash_diff = sourceSat.hash_diff"
+        if DeltaTable.isDeltaTable(spark, output_path):
+            delta_table = DeltaTable.forPath(spark, output_path)
+            target_df = delta_table.toDF()
+            same_count = sat_df.count() == target_df.count()
+            same_keys = sat_df.select(f"{tableName}_HK").subtract(target_df.select(f"{tableName}_HK")).isEmpty()
+
+            if same_count and same_keys:
+                logger.info(f"No changes detected for Sat_{satName}. Skipping merge.")
+                return
+            delta_table.alias("target").merge(
+                source=sat_df.alias("sourceSat"),
+                condition=expr(condition)
+            ).whenNotMatchedInsertAll().execute()
+            logger.debug(f"Satellite {satName} successfully updated/merged!")
+        else:        
+            sat_df.write.format("delta").mode("overwrite").save(output_path)    
+            logger.debug(f"Satellite {satName} successfully created!")
     except Exception as e:
         logger.error(f"Satellite {satName} creating failed...", exc_info=True)
         raise
---
+++ time_travel.py
@@ -0,0 +1,31 @@
+from pyspark.sql import SparkSession
+from delta.tables import DeltaTable
+import os
+
+spark = SparkSession.builder \
+    .appName("Hubs-Links-Satellites Generator") \
+    .config("spark.hadoop.hadoop.security.access.control.only.use.native", "false") \
+    .getOrCreate()
+
+def read_delta_time_travel(path, version=None, timestamp=None):
+    reader = spark.read.format("delta")
+
+    if version is not None:
+        reader = reader.option("versionAsOf", version)
+    elif timestamp is not None:
+        reader = reader.option("timestampAsOf", timestamp)
+
+    df = reader.load(path)
+    return df
+
+hub_customer_path = os.path.join("C:\Program Files\sudarshan\cs_ecommerce_data_vault", "delta", "vault", "sat_customer")
+delta_table = DeltaTable.forPath(spark, hub_customer_path)
+history_df = delta_table.history()  
+history_df.show(truncate=True)
+
+v_n = int(input("Enter version number: "))
+
+historical_hub_df = read_delta_time_travel(hub_customer_path, version=v_n)
+historical_hub_df.show()
+historical_hub_df = read_delta_time_travel(hub_customer_path, version=v_n)
+historical_hub_df.show()
